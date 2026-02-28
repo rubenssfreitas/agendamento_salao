192a5a5a1f3e88072eb64b296b121ad1de24b086
Rubens Freitas <rubens.suporte@gmail.com>
2026-02-28 20:38:27 -0300

    connect mongo
    conect banco dados

---
+++ src/database/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cursojs.kmiopqb.mongodb.net/database?appName=cursojs`,
+    (error) => {
+      if (error) {
+        return console.error("Erro ao conectar ao banco de dados:", error);
+      }
+      return console.log("Conexão com o banco de dados estabelecida com sucesso!");
+    },
+  );
+};
+
+
+ 
+module.exports = connectToDatabase;
