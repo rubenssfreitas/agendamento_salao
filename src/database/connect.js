15eebffeac1700f565796cd4256d29a9b0f0ba9e
Rubens Freitas <rubens.suporte@gmail.com>
2026-02-28 20:45:03 -0300

    atualiza
    atualiza

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cursojs.kmiopqb.mongodb.net/database?appName=cursojs`,
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@bancocusronode.azaky5h.mongodb.net/?appName=bancoCusroNode`,
     (error) => {
       if (error) {
         return console.error("Erro ao conectar ao banco de dados:", error);
