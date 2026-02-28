72f30da9fc9d878352a9924cc18d4b2b2524432a
Rubens Freitas <rubens.suporte@gmail.com>
2026-02-28 12:02:09 -0300

    mongoose
    ok

--- modules/express.js
+++ modules/express.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/home', (req, res) => {
+    res.contentType('text/html');
   res.status(200).send('<h1>Hello World!</h1>');
 });
+
+app.get('/users', (req, res) => { 
+    const users = [
+            { name: 'Alice', email: 'alice@example.com' },
+            { name: 'Bob', email: 'bob@example.com' },
+        ];
+    res.status(200).json(users);
+    });
 
 const port = 8080;
 
