299c45f53d5f435e8f591dc9c60b48bd31176695
Rubens Freitas <rubens.suporte@gmail.com>
2026-03-03 17:31:36 -0300

    em casa
    casa

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,17 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const connectToDatabase = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@bancocusronode.azaky5h.mongodb.net/?appName=bancoCusroNode`,
-    (error) => {
-      if (error) {
-        return console.error("Erro ao conectar ao banco de dados:", error);
-      }
-      return console.log("Conexão com o banco de dados estabelecida com sucesso!");
-    },
-  );
-};
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@bancocusronode.azaky5h.mongodb.net/?appName=bancoCusroNode`)
+    console.log('✅ Banco conectado com sucesso')
+  } catch (error) {
+    console.error('❌ Erro ao conectar no banco:', error)
+  }
+}
 
-
-module.exports = connectToDatabase;
+module.exports = connectToDatabase
 
